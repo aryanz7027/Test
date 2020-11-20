55fd7a973a8486e182fde12a49b9e873d762292e
Garima Sharma <garimasharma4998@gmail.com>
2020-11-20 14:36:33 +0530

    Ruby program to add integers
    This program facilitates the user to add any number of integers by using
    the concept of class in Ruby. It give the usage of class,object,
    attr_accessor, taking input in form of string and converting to integer
    array and then passing to attr_accessor.

--- Addition.rb
+++ Addition.rb
@@ -1,16 +1,17 @@
+#Program for addition of numbers
+
 class Addition
-  def  initialize(arg)  
-     @arr = arg  
-  end 
+  attr_accessor :arr          		#using attr_accessor to access the var
 
   def print
-    @sum = @arr.sum
-    puts 'Result of adding  numbers is : ' + @sum.to_s
+    sum = arr.sum
+    puts 'Result of adding  numbers is : ' + sum.to_s
   end
 end
 
 
 puts 'Enter the numbers for addition : '
-arg = gets.split(' ').map &:to_i
-obj = Addition.new(arg)
+arg = gets.split(' ').map &:to_i		#converting array entries separated by space into integers	
+obj = Addition.new			#initializing class obj
+obj.arr = arg
 obj.print
